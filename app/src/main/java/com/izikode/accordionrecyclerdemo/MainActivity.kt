8122a924793578a1aa4c1dd27d368419a8e800a4
iFanie <fanieveizis@gmail.com>
2018-11-12 23:23:09 +0200

    Cleanup code and build files

--- app/src/main/java/com/izikode/accordionrecyclerdemo/MainActivity.kt
+++ app/src/main/java/com/izikode/accordionrecyclerdemo/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.izikode.accordionrecyclerdemo
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import com.izikode.accordionrecyclerdemo.data.*
 import kotlinx.android.synthetic.main.activity_main.*
