4b49fae661d69f3b4661896c4be191fbbe435dbc
iFanie <fanieveizis@gmail.com>
2018-11-15 21:12:43 +0200

    Create recycling details data class

---
+++ accordionrecycler/src/main/java/com/izikode/izilib/accordionrecycler/AccordionRecyclerItemDetails.kt
@@ -0,0 +1,28 @@
+package com.izikode.izilib.accordionrecycler
+
+/**
+ * Data class containing additional information about an item being recycled.
+ */
+data class AccordionRecyclerItemDetails(
+
+    /**
+     * The number of immediate children of the current item.
+     */
+    val numberOfImmediateEnclosedItems: Int,
+
+    /**
+     * The number of total children, immediate or not, of the current item.
+     */
+    val numberOfTotalEnclosedItems: Int,
+
+    /**
+     * The overall position info of the current item.
+     */
+    val overallPosition: AccordionRecyclerPosition,
+
+    /**
+     * The position info of the current item, relative to it's parent item.
+     */
+    val enclosedPosition: AccordionRecyclerPosition
+
+)
